d8750bdf2136fb5c21787da9887ee8dc1e992cc6
Jim Nelson <jnelson@archive.org>
2019-02-21 13:33:21 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Kleptes: Basic work stealing framework for Redis
+
+* Jim Nelson <<jnelson@archive.org>>
+* Internet Archive
+* Presented at RedisConf 2019
+
+## Introduction
+
+(tbd)
+
+## License
+
+Kleptes is licensed under the [GNU Affero General Public License](https://www.gnu.org/licenses/agpl.html|).  See the LICENSE file for more information.
+
+## Unit tests
+
+(tbd)
+
+## More information
+
+* [Work stealing (Wikipedia)](https://en.wikipedia.org/wiki/Work_stealing)
---
+++ src/Kleptes/Autoloader.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Copyright (C) 2019 Internet Archive
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Kleptes;
+
+/**
+ * Quick-and-dirty autoloader for Kleptes library.
+ */
+class Autoloader
+{
+  /**
+   * Register Kleptes' autoloader.
+   */
+  public static function register()
+  {
+    spl_autoload_register(function ($class) {
+      if (strpos($class, 'Kleptes\\') !== 0)
+        return;
+
+      $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+      $path = "src/$path.php";
+
+      if (is_file($path))
+        require_once $path;
+    });
+  }
+}
